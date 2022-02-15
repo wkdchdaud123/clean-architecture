177b499bff274e963433c4db6e74ed2135dd81bd
chongmyung.jang <chongmyung.jang@linecorp.com>
2022-02-15 21:05:13 +0900

    remove test api

--- src/main/java/com/study/hexagonal/StudyHexagonalApplication.java
+++ src/main/java/com/study/hexagonal/StudyHexagonalApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class StudyHexagonalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyHexagonalApplication.class, args);
-    }
-
-    @GetMapping
-    public String test(){
-        return "test";
     }
 
 }
